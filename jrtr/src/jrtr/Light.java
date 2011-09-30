b7464bdeaf43a3b85d40918edb4d8caa2c26239d
Aaron Karper <akarper@students.unibe.ch>
2011-09-30 22:31:57 +0200

    New setup

--- jrtr/src/jrtr/Light.java
+++
@@ -1,8 +0,0 @@
-package jrtr;
-
-/**
- * Stores the properties of a light source. To be implemented for 
- * the "Texturing and Shading" project.
- */
-public class Light {
-}
--- jrtr/src/jrtr/Material.java
+++
@@ -1,8 +0,0 @@
-package jrtr;
-
-/**
- * Stores the properties of a material. You will implement this 
- * class in the "Shading and Texturing" project.
- */
-public class Material {
-}
